5116e885a542323f54b63d789daec277502dfa0f
William Millette <wmillett@c2r2p6.42quebec.com>
2024-04-13 16:35:01 -0400

    prep to merge

--- ui_prototype/event_popup/event_button1.gd
+++ ui_prototype/event_popup/event_button1.gd
@@ -1,20 +1,21 @@
-extends Window
-signal alex
+extends Button
+signal b1_pressed
+
+
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#self.hide()
 	pass # Replace with function body.
+
+
+#func _on_Button_pressed():
+	#print("hello")
+	#pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_event_first_button_pressed():
-	print("test")
-	self.hide()
-	alex.emit()
-	pass # Replace with function body.
 
---
+++ ui_prototype/event_popup/event_window1.gd
@@ -0,0 +1,24 @@
+extends Window
+signal event_window_done
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#self.hide()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+
+
+func _on_event_button_2_b_2_pressed():
+	
+	pass # Replace with function body.
+
+
+func _on_event_button_1_b_1_pressed():
+	pass # Replace with function body.
