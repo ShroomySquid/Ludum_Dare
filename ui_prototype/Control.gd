2358822b6bf0f69bddf34eea1bc63ffeb82ba50c
Leopold Couturier-Otis <lcouturi@c2r2p9.42quebec.com>
2024-04-13 13:48:09 -0400

    magic loss depends on amount of magic people

--- ui_prototype/Control.gd
+++ ui_prototype/Control.gd
@@ -1,8 +1,10 @@
 extends Control
-var cultist_count = 100
+var available_cultists = 10
+var total_cultists = 10
 var magic_count = 1000
 var loyalty_percent = 100
 var prisoners_count = 0
+var cultists_in_ritual = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,10 +14,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	magic_count -= delta * cultist_count
+	if (magic_count > cultists_in_ritual * 100):
+		magic_count -= delta * (total_cultists - cultists_in_ritual)
 	if (int(magic_count) == 0):
 		print("Dead you are, try again you must.")
-	$cultist_label.text = "Cultists: " + str(cultist_count)
+	if (cultists_in_ritual > total_cultists):
+		$Cultist_Send.value = total_cultists
+	available_cultists = total_cultists - cultists_in_ritual
+	$cultist_label.text = "Cultists: " + str(available_cultists)
 	$magic_label.text = "Magic: " + str(int(magic_count))
 	$loyalty_bar.value = loyalty_percent
 	$prisoners_label.text = "Prisoners: " + str(prisoners_count)
+	cultists_in_ritual = $Cultist_Send.value
