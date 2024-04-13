851d08b2ee766be52703e8955343d6972104df7f
Leopold Couturier-Otis <lcouturi@c2r2p9.42quebec.com>
2024-04-13 12:59:06 -0400

    more UI

--- ui_prototype/Control.gd
+++ ui_prototype/Control.gd
@@ -1,7 +1,8 @@
 extends Control
-var cultist_count = 0
-var magic_count = 0
+var cultist_count = 100
+var magic_count = 1000
 var loyalty_count = 0
+var prisoners_count = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,3 +15,4 @@ func _process(delta):
 	$cultist_label.text = "Cultists: " + str(cultist_count)
 	$magic_label.text = "Magic: " + str(magic_count)
 	$loyalty_bar.value = loyalty_count
+	$prisoners_label.text = "Prisoners: " + str(prisoners_count)
