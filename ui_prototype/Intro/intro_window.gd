3941835858a3846b3b89f8c51e8b0da0cd2fd611
William Millette <wmillett@c2r2p7.42quebec.com>
2024-04-14 20:33:58 -0400

    intro window now pulls text from txt

--- ui_prototype/Intro/intro_window.gd
+++ ui_prototype/Intro/intro_window.gd
@@ -1,22 +1,99 @@
 extends Window
 
+const ALL_TXT = 3
+
+var file_paths = [
+	'res://Intro/intro_texts/intro1.txt',
+	'res://Intro/intro_texts/intro2.txt',
+	'res://Intro/intro_texts/intro3.txt'
+]
+
+var i = 0
+var texts = [
+	"",
+	"",
+	""
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.add_theme_icon_override("close", Texture2D.new())
-
-
+	
+	load_file(file_paths)
+	#var content = ""
+	#var i = 0
+	#self.add_theme_icon_override("close", Texture2D.new())
+	#if file.file_exists(file_paths[i]):
+		#var error = file.open(file_paths[i], File.READ)
+		#if error == OK:m
+			## Read the content of the file
+			#content = file.get_as_text()
+			#file.close()
+		#else:
+			#print("Error opening file:", file_paths[i])
+	#else:
+		#print("File not found:", file_paths[i])
+	var label_node = get_node("intro_label")
+	if label_node != null:
+		label_node.text = texts[0]
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _on_s_key_pressed():
-	self.hide()
+#func _on_s_key_pressed():
+	#self.hide()
+	#pass
+
+#func load_file(file_paths):
+	#var file
+	#var j = 0
+	#while j < ALL_TXT:
+		#file = FileAccess.open(file_paths[j], FileAccess.READ)
+		#texts[j] += file.get_as_text()
+		#j += 1
+	#pass
+	
+func load_file(file_paths: Array) -> void:
+	var j = 0
+	while j < file_paths.size():
+		var file = FileAccess.open(file_paths[j], FileAccess.READ)
+		var file_path = file_paths[j]
+		#var error = file.open(file_paths[j], FileAccess.READ)
+		#if error != OK:
+			#print("Error opening file:", file_path)
+			## Handle error (e.g., skip file, break loop, etc.)
+			#j += 1
+			#continue
+		
+		# Read file content
+		var file_content = file.get_as_text()
+		texts[j] += file_content
+		
+		# Close the file
+		file.close()
+
+		j += 1
 	pass
 
 
-
 func _on_button_pressed():
+	i += 1
+	if i >= ALL_TXT:
+		get_parent().get_node("UI").is_paused = false
+		get_parent().get_node("UI").in_intro = false
+		self.hide()
+		pass
+	else:
+		var label_node = get_node("intro_label")
+		if label_node != null:
+			label_node.text = texts[i]
+		pass
+		
+
+
+
+func _on_skip_button_pressed():
 	get_parent().get_node("UI").is_paused = false
 	get_parent().get_node("UI").in_intro = false
 	self.hide()
+	pass
